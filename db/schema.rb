83be5a357b636417341537c32d9a257351157417
dooma <calinutz96@yahoo.com>
2012-08-13 14:46:20 +0300

    remember me working

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120813083344) do
+ActiveRecord::Schema.define(:version => 20120813111524) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
+
+  add_index "users", ["remember_me_token"], :name => "index_users_on_remember_me_token"
 
 end
