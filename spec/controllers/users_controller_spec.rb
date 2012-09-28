06288dc10893003582fabad75c389316e284d316
Calin <calinutz96@yahoo.com>
2012-09-28 22:00:05 +0300

    add more tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,5 +2,48 @@ require 'spec_helper'
 
 describe UsersController do
   
+  before(:each) do
+    @attr = { 
+      :username => "foobar",
+      :email => "foo@bar.com",
+      :password => "passw123",
+      :password_confirmation => "passw123"
+    }
+  end
 
+  describe "Test require_login filter" do
+    it "should require login" do
+      user = FactoryGirl.create(:user)
+      get :show, :id => user.id
+      response.should redirect_to(root_path)
+    end
+    it "should require login" do
+      user = FactoryGirl.create(:user)
+      delete :destroy, :id => user.id
+      response.should redirect_to(root_path)
+    end
+  end
+  
+  it "should get 200" do
+    get :new
+    response.status.should eq(200)
+  end
+
+  it "should notice if user is created" do
+    post :create, :user => @attr
+    flash.now[:notice].should eq("User was successfully created. Please activate email first!")
+    user = User.last
+    user.should_not be_nil
+    user.username.should eq('foobar')
+    user.activation_state.should eq('pending')
+    user.email.should eq('foo@bar.com')
+  end
+
+  it "should render :new method if user can't be saved" do
+    post :create, :user => @attr.merge(:username => "")
+    response.should render_template('new')
+    
+    post :create, :user => @attr.merge(:password_confirmation => "")
+    response.should render_template('new')
+  end
 end
--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -11,13 +11,24 @@ describe "UserSessions" do
   end
 
   describe "POST /user_sessions" do
-    it "should return error message if user does not exist" do
+    it "should return error message if user does not exist or credentials are not valid" do
       visit new_user_sessions_path
       fill_in 'Username', :with => 'example@example.com'
       fill_in 'Password', :with => 'parola'
       click_button 'Login'
 
       page.should have_content('Login unsuccessful')
+    end
+
+    it "should login if user credentials are valid" do
+      # Create user
+      @user = FactoryGirl.create(:user)
+      @user.activate!
+      visit new_user_sessions_path
+      fill_in 'Username', :with => @user.username
+      fill_in 'Password', :with => "parola123"
+      click_button 'Login'
+      page.should have_content("Login successful")
     end
   end
 end
