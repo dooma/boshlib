04eaf05668620e7ae4a7aec88dda0fa0474564e2
Calin <calinutz96@yahoo.com>
2012-09-29 18:16:18 +0300

    add test for USERS controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -32,6 +32,7 @@ describe UsersController do
   it "should notice if user is created" do
     post :create, :user => @attr
     flash.now[:notice].should eq("User was successfully created. Please activate email first!")
+    response.should redirect_to(root_path)
     user = User.last
     user.should_not be_nil
     user.username.should eq('foobar')
@@ -45,5 +46,38 @@ describe UsersController do
     
     post :create, :user => @attr.merge(:password_confirmation => "")
     response.should render_template('new')
+  end
+
+  it "should activate user if token is correct" do
+    @user = User.new(@attr)
+    @user.save if @user.valid?
+    @user.activation_token.should_not be_nil
+    @user.activation_state.should eq("pending")
+    
+    post :activate, :id => @user.activation_token
+    flash.now[:notice].should eq("Activation successfully")
+    response.should redirect_to(root_path)
+
+    user = User.where(:username => "foobar").first
+    user.activation_token.should be_nil
+    user.activation_state.should eq("active")
+  end
+
+  it "should redirect and show message if user can't be activated" do
+    @user = User.new(@attr)
+    @user.save if @user.valid?
+    token = @user.activation_token
+    @user.activate!
+    @user.activation_state.should eq("active")
+    @user.activation_token.should be_nil
+    
+    post :activate, :id => token
+    flash.now[:alert].should eq("Account can not be activated")
+    response.should redirect_to(root_path)
+
+    # If user does not exist
+    post :activate, :id => BCrypt::Password.create(Time.now)
+    flash.now[:alert].should eq("Account can not be activated")
+    response.should redirect_to(root_path)
   end
 end
