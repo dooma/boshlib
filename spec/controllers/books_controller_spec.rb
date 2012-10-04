c71f1808fc80c0e8e6d7b1d344f02745fc415a22
John Doe <calinutz96@yahoo.com>
2012-10-04 22:01:26 +0300

    add tests for update and destroy methods

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -14,6 +14,7 @@ describe BooksController do
       response.status.should eq(200)
     end
   end
+
   describe "POST methods" do
     it "should create new book in database" do
       post :create, :book => {:title => "x", :description => "y", :price => "12"}
@@ -25,10 +26,40 @@ describe BooksController do
       flash.now[:notice].should eq('Book was successfully created.')
     end
 
-    it "should render page if book can't be saved" do
+    it "should render new page if book can't be saved" do
       post :create, :book => {:title => "x", :description => "y"}
       Book.all.should be_empty
-      response.should render_template(:new)#"new")
+      response.should render_template(:new)
+    end
+
+    it "should update book properties" do
+      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      book = Book.last
+      book.title.should eq("x")
+      post :update, :id => book.id, :book => {:title => "z"}
+      book = Book.last
+      book.title.should eq("z")
+      response.should redirect_to(book)
+      flash.now[:notice].should eq('Book was successfully updated.')
+    end
+
+    it "should render edit page if book can't be updated" do
+      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      book = Book.last
+      book.title.should eq("x")
+      post :update, :id => book.id, :book => {:title => ""}
+      book = Book.last
+      book.title.should_not be_nil
+      book.title.should eq("x")
+      response.should render_template(:edit)
+    end
+  end
+
+  describe "PUT methods" do
+    it "should destroy book" do
+      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      put :destroy, :id => Book.last.id
+
     end
   end
 end
