6963b23037d7d4ae8611763902f9422f4046584e
Calin <calinutz96@yahoo.com>
2012-10-07 22:29:08 +0300

    refactor tests after change validations

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -8,7 +8,7 @@ describe BooksController do
       get :new
       response.status.should eq(200)
       
-      book = Book.new(:title => "x", :description => "y", :price => "12")
+      book = Book.new(:title => "xzf", :description => "y", :price => "12")
       book.save if book.valid?
       get :show, :id => book.id
       response.status.should eq(200)
@@ -17,9 +17,9 @@ describe BooksController do
 
   describe "POST methods" do
     it "should create new book in database" do
-      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      post :create, :book => {:title => "xzf", :description => "y", :price => "12"}
       book = Book.last
-      book.title.should eq("x")
+      book.title.should eq("xzf")
       book.description.should eq("y")
       book.price.should eq("12")
       response.should redirect_to(book)
@@ -33,33 +33,32 @@ describe BooksController do
     end
 
     it "should update book properties" do
-      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      post :create, :book => {:title => "xzf", :description => "y", :price => "12"}
       book = Book.last
-      book.title.should eq("x")
-      post :update, :id => book.id, :book => {:title => "z"}
+      book.title.should eq("xzf")
+      post :update, :id => book.id, :book => {:title => "znoob"}
       book = Book.last
-      book.title.should eq("z")
+      book.title.should eq("znoob")
       response.should redirect_to(book)
       flash.now[:notice].should eq('Book was successfully updated.')
     end
 
     it "should render edit page if book can't be updated" do
-      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      post :create, :book => {:title => "xzf", :description => "yzf", :price => "12"}
       book = Book.last
-      book.title.should eq("x")
+      book.title.should eq("xzf")
       post :update, :id => book.id, :book => {:title => ""}
       book = Book.last
       book.title.should_not be_nil
-      book.title.should eq("x")
+      book.title.should eq("xzf")
       response.should render_template(:edit)
     end
   end
 
   describe "PUT methods" do
     it "should destroy book" do
-      post :create, :book => {:title => "x", :description => "y", :price => "12"}
+      post :create, :book => {:title => "xzf", :description => "y", :price => "12"}
       put :destroy, :id => Book.last.id
-
     end
   end
 end
