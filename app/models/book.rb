156147d249d22a73373ec309bf40aa8bb4f168c2
Calin <calinutz96@yahoo.com>
2012-10-21 16:18:45 +0300

    optimize validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :title, :year, :description, :price
-  validates :title, :presence => true, :length => {:minimum => 3}
+  validates :title, :presence => true, :length => {:minimum => 3, :message => "Title too short"} 
   validates :description, :presence => true
-  validates :price, :presence => true, :format => {:with => /^?[0-9]*\.?[0-9]*$/}
+  validates :price, :presence => true, :format => {:with => /^[0-9]*\.?[0-9]*$/, :message => "Price should be float number"} 
 end
